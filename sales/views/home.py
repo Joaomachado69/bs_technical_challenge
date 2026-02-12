e7772ad6ce5890b276f7a543b9289978e7571751
Joaomachado69 <machado.joo0@gmail.com>
2026-02-12 01:56:12 -0400

    complete stage 6

--- sales/views/home.py
+++ sales/views/home.py
@@ -1,7 +1,13 @@
 from django.template.response import TemplateResponse
+from sales.models import Cart
+from django.db.models import Sum
 
 __all__ = ["home_view"]
-
-
 def home_view(request):
-    return TemplateResponse(request, "sales/home.html", {})
+    cart_id = request.session.get('cart_id')
+    cart = Cart.objects.filter(id=cart_id).first() if cart_id else None
+    cart_item_count = cart.items.aggregate(total=Sum('quantity'))['total'] or 0 if cart else 0
+    
+    return TemplateResponse(request, "sales/home.html", {
+        "cart_item_count": cart_item_count
+    })
